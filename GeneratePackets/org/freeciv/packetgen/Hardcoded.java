b7fe240c9b8b427e804e1f4790fd63f535a7d0f9
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2011-11-07 17:43:56 +0100

    Improved Hardcoded data (but it's still hardcoded)

--- GeneratePackets/org/freeciv/packetgen/Hardcoded.java
+++ GeneratePackets/org/freeciv/packetgen/Hardcoded.java
@@ -1,34 +1,50 @@
 package org.freeciv.packetgen;
 
+import java.util.HashMap;
+
 //TODO: Move data to file
 public class Hardcoded {
-        public static final JavaSrc[] out = {
-                new JavaSrc("uint32(int)",
-                        "Long",
-                        DataIO.readUIntCode(4, "Long", "value"),
-                        DataIO.writeWriteUInt(4),
-                        "return 4;"),
-                new JavaSrc("string(char)",
-                        "String",
-                        "StringBuffer buf = new StringBuffer();\n" +
-                                "\t\tbyte letter = from.readByte();\n" +
-                                "\t\twhile (letter != 0) {\n" +
-                                "\t\t\tbuf.append((char)letter);\n" +
-                                "\t\t\tletter = from.readByte();\n" +
-                                "\t\t}\n" +
-                                "\t\tvalue = buf.toString();",
-                        "to.writeBytes(" + "value" + ");\n" +
-                            "\t\t" + "to.writeByte(0);" + "\n",
-                        "return " + "value" + ".length() + 1;\n"),
-                new JavaSrc("bool8(bool)",
-                        "Boolean",
-                        "value = from.readBoolean();" + "\n",
-                        "to.writeBoolean(value);" + "\n",
-                        "return 1;"),
-                new JavaSrc("sint16(int)",
-                        "Short",
-                        "value = from.readShort();" + "\n",
-                        "to.writeShort(value);" + "\n",
-                        "return 2;")
-        };
+    private HashMap<String, JavaSrc> data = new HashMap<String, JavaSrc>();
+    private static Hardcoded instance;
+
+    private Hardcoded() {
+        for (JavaSrc src: new JavaSrc[]{
+            new JavaSrc("uint32(int)",
+                    "Long",
+                    DataIO.readUIntCode(4, "Long", "value"),
+                    DataIO.writeWriteUInt(4),
+                    "return 4;"),
+            new JavaSrc("string(char)",
+                    "String",
+                    "StringBuffer buf = new StringBuffer();\n" +
+                            "\t\tbyte letter = from.readByte();\n" +
+                            "\t\twhile (letter != 0) {\n" +
+                            "\t\t\tbuf.append((char)letter);\n" +
+                            "\t\t\tletter = from.readByte();\n" +
+                            "\t\t}\n" +
+                            "\t\tvalue = buf.toString();",
+                    "to.writeBytes(" + "value" + ");\n" +
+                        "\t\t" + "to.writeByte(0);" + "\n",
+                    "return " + "value" + ".length() + 1;\n"),
+            new JavaSrc("bool8(bool)",
+                    "Boolean",
+                    "value = from.readBoolean();" + "\n",
+                    "to.writeBoolean(value);" + "\n",
+                    "return 1;"),
+            new JavaSrc("sint16(int)",
+                    "Short",
+                    "value = from.readShort();" + "\n",
+                    "to.writeShort(value);" + "\n",
+                    "return 2;")
+        }) {
+            data.put(src.getCSrc(), src);
+        }
+    }
+
+    public static JavaSrc getJTypeFor(String src) {
+        if (null == instance) {
+            instance = new Hardcoded();
+        }
+        return instance.data.get(src);
+    }
 }
