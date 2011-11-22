a78c4fc1d1ac298146ae7e0a0cce563be62410d8
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2011-11-22 17:18:23 +0100

    GeneratePackages: Added GeneratePackages. At the moment it only reads field types.

--- GeneratePackets/org/freeciv/packetgen/GeneratePackets.scala
+++ GeneratePackets/org/freeciv/packetgen/GeneratePackets.scala
@@ -1,7 +1,31 @@
 package org.freeciv.packetgen
 
 import util.parsing.combinator._
-import util.parsing.input.Reader
+import java.io.{InputStreamReader, FileInputStream, IOException, File}
+import util.parsing.input.{StreamReader, Reader}
+
+class GeneratePackets(packetsDefPath: File, devMode: Boolean) {
+
+  def this(packetsDefPath: String, devMode: Boolean) = this(new File(packetsDefPath), devMode)
+
+  private def storage = new PacketsStore(devMode)
+  private def Parser = new ParsePacketsDef(storage)
+
+  if (!packetsDefPath.exists()) {
+    throw new IOException(packetsDefPath.getAbsolutePath + " doesn't exist.")
+  } else if (!packetsDefPath.canRead()) {
+    throw new IOException("Can't read " + packetsDefPath.getAbsolutePath)
+  }
+
+  val toReadFrom: StreamReader = StreamReader(new InputStreamReader(new FileInputStream(packetsDefPath)))
+  Parser.parsePacketsDef(toReadFrom)
+}
+
+object GeneratePackets {
+  def main(args: Array[String]) {
+    val self = new GeneratePackets(args(0), true)
+  }
+}
 
 class ParsePacketsDef(storage: PacketsStore) extends RegexParsers {
   def fieldType = regex("""[A-Z](\w|_)*""".r)
