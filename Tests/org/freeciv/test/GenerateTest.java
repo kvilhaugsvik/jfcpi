a670627b3578a85b9bdb358b774d295eb2cc2637
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2011-12-02 13:43:55 +0100

    Add copyright notices to source code

--- Tests/org/freeciv/test/GenerateTest.java
+++ Tests/org/freeciv/test/GenerateTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2011. Sveinung Kvilhaugsvik
+ * Portions are data from Freeciv's common/packets.def. Copyright
+ * to those (if copyrightable) not claimed by Sveinung Kvilhaugsvik
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package org.freeciv.test;
 
 import org.freeciv.packetgen.Packet;
