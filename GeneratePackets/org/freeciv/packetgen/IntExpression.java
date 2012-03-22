dede462759d9f4e58129b9249db5c91cc5ef67de
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2012-03-22 02:41:23 +0100

    GeneratePackets: Refactor IntExpression internally so instance functions passes requirements to the constructor.

--- GeneratePackets/org/freeciv/packetgen/IntExpression.java
+++ GeneratePackets/org/freeciv/packetgen/IntExpression.java
@@ -10,13 +10,13 @@ public class IntExpression {
     private final IntExpression rhs;
     private final HashSet reqs;
 
-    private IntExpression(String operatorOrValue, IntExpression lhs, IntExpression rhs, boolean iDemand) {
+    private IntExpression(String operatorOrValue, IntExpression lhs, IntExpression rhs, Requirement... iDemand) {
         this.operatorOrValue = operatorOrValue;
         this.lhs = lhs;
         this.rhs = rhs;
         this.reqs = new HashSet();
-        if (iDemand)
-            reqs.add(new Requirement(operatorOrValue, Requirement.Kind.VALUE));
+        for (Requirement req : iDemand)
+            reqs.add(req);
         if (null != lhs)
             reqs.addAll(lhs.getReqs());
         if (null != rhs)
@@ -44,23 +44,23 @@ public class IntExpression {
     }
 
     public static IntExpression binary(String operator, IntExpression lhs, IntExpression rhs) {
-        return new IntExpression(operator, lhs, rhs, false);
+        return new IntExpression(operator, lhs, rhs);
     }
 
     public static IntExpression unary(String operator, IntExpression rhs) {
-        return new IntExpression(operator, null, rhs, false);
+        return new IntExpression(operator, null, rhs);
     }
 
     public static IntExpression suf(IntExpression lhs, String operator) {
-        return new IntExpression(operator, lhs, null, false);
+        return new IntExpression(operator, lhs, null);
     }
 
     public static IntExpression integer(String value) {
-        return new IntExpression(value, null, null, false);
+        return new IntExpression(value, null, null);
     }
 
     public static IntExpression variable(String name) {
-        return new IntExpression(name, null, null, true);
+        return new IntExpression(name, null, null, new Requirement(name, Requirement.Kind.VALUE));
     }
 
     public Collection<Requirement> getReqs() {
