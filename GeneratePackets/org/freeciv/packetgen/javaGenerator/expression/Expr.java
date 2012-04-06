1ae1bf9b2f85f5c57a785f0d175a3b9e776b1ce2
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2012-04-06 20:26:44 +0200

    GeneratePackets: Add representation for a value that must be of a certain type and for expressions.

---
+++ GeneratePackets/org/freeciv/packetgen/javaGenerator/expression/Expr.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2012. Sveinung Kvilhaugsvik
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.freeciv.packetgen.javaGenerator.expression;
+
+/**
+ * An expression that returns a value represented by Returns
+ * @param <Returns> The kind of value it returns
+ */
+public interface Expr<Returns extends TypedValueCode> {
+    /**
+     * Source code for an expression that will return a value of the type represented by Returns
+     * @return The source code
+     */
+    public Returns getCodeFor();
+
+    @Override
+    public abstract String toString();
+}
