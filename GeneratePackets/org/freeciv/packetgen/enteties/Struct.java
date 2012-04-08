f0adcc3663a42b5819ade7b40073e312084115a0
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2012-04-09 01:04:18 +0200

    GenereatePackets: Add class writer for struct

---
+++ GeneratePackets/org/freeciv/packetgen/enteties/Struct.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2012. Sveinung Kvilhaugsvik
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.freeciv.packetgen.enteties;
+
+import org.freeciv.packetgen.javaGenerator.ClassWriter;
+import org.freeciv.types.FCEnum;
+
+import java.util.*;
+
+public class Struct extends ClassWriter {
+    public Struct(String name, List<Map.Entry<String, String>> fields) {
+        super(ClassKind.CLASS,
+                new TargetPackage(FCEnum.class.getPackage()),
+                new String[]{}, "Freeciv C code", name, null, null);
+
+        addConstructorParameterToFields(name, fields);
+
+        for (Map.Entry<String, String> field: fields) {
+            addObjectConstant(field.getKey(), field.getValue());
+            addMethodPublicReadObjectState(null, field.getKey(), "get" + field.getValue(),
+                    "return " + field.getValue() + ";");
+        }
+    }
+
+    private void addConstructorParameterToFields(String name, List<Map.Entry<String, String>> fields) {
+        String[] body = new String[fields.size()];
+        for (int i = 0; i < fields.size(); i++) {
+            body[i] = setFieldToVariableSameName(fields.get(i).getValue());
+        }
+        addConstructorPublic(null, name, createParameterList(fields), body);
+    }
+}
