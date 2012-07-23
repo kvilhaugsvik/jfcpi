731ca14e3e9699d9579cc7c3610f1edc790d6b52
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2012-07-23 04:30:09 +0200

    GeneratePackets: Add CodeAtom a representation for a small code element.
    Don't call it token as its not yet fine grained enough.

---
+++ GeneratePackets/org/freeciv/packetgen/javaGenerator/CodeAtoms.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2012. Sveinung Kvilhaugsvik
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.freeciv.packetgen.javaGenerator;
+
+import org.freeciv.Util;
+
+import java.util.LinkedList;
+
+public class CodeAtoms {
+    private final LinkedList<CodeAtom> atoms = new LinkedList<CodeAtom>();
+    private Util.Does<CodeAtom> reason = null;
+
+    public void add(CodeAtom atom) {
+        if (null == reason || !reason.holdFor(atom))
+            atoms.add(atom);
+        reason = null;
+    }
+
+    public void refuseNextIf(Util.Does<CodeAtom> reason) {
+        this.reason = reason;
+    }
+
+    CodeAtom[] getAtoms() {
+        return atoms.toArray(new CodeAtom[0]);
+    }
+}
---
+++ GeneratePackets/org/freeciv/packetgen/javaGenerator/HasAtoms.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2012. Sveinung Kvilhaugsvik
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.freeciv.packetgen.javaGenerator;
+
+public interface HasAtoms {
+    public static final CodeAtom EOL = new CodeAtom(";");
+    public static final CodeAtom LSC = new CodeAtom("{");
+    public static final CodeAtom RSC = new CodeAtom("}");
+    public static final CodeAtom LPR = new CodeAtom("(");
+    public static final CodeAtom RPR = new CodeAtom(")");
+
+    public static final CodeAtom IF = new CodeAtom("if");
+    public static final CodeAtom ELSE = new CodeAtom("else");
+
+    public void writeAtoms(CodeAtoms to);
+}
