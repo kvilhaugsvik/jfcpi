d6b97831c2364a783ecf59960bd200f917a9a145
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2012-09-02 06:32:40 +0200

    GeneratePackets: Enteties: SimpleTypeAlias: Move field type basic constructor to the more typed one.

--- GeneratePackets/org/freeciv/packetgen/enteties/supporting/SimpleTypeAlias.java
+++ GeneratePackets/org/freeciv/packetgen/enteties/supporting/SimpleTypeAlias.java
@@ -17,8 +17,17 @@ package org.freeciv.packetgen.enteties.supporting;
 import org.freeciv.packetgen.dependency.IDependency;
 import org.freeciv.packetgen.dependency.Requirement;
 import org.freeciv.packetgen.enteties.FieldTypeBasic;
+import org.freeciv.packetgen.javaGenerator.MethodCall;
+import org.freeciv.packetgen.javaGenerator.Var;
+import org.freeciv.packetgen.javaGenerator.expression.Block;
+import org.freeciv.packetgen.javaGenerator.expression.creators.ExprFrom1;
+import org.freeciv.packetgen.javaGenerator.expression.creators.ExprFrom2;
+import org.freeciv.packetgen.javaGenerator.expression.util.BuiltIn;
+import org.freeciv.packetgen.javaGenerator.expression.willReturn.AValue;
 
 import java.util.*;
+
+import static org.freeciv.packetgen.javaGenerator.expression.util.BuiltIn.asAValue;
 
 public class SimpleTypeAlias implements IDependency, FieldTypeBasic.Generator {
     private final Requirement iProvide;
@@ -32,13 +41,27 @@ public class SimpleTypeAlias implements IDependency, FieldTypeBasic.Generator {
     }
 
     @Override
-    public FieldTypeBasic getBasicFieldTypeOnInput(NetworkIO io) {
+    public FieldTypeBasic getBasicFieldTypeOnInput(final NetworkIO io) {
         return new FieldTypeBasic(io.getIFulfillReq().getName(), iProvide.getName(),
                                   typeInJava,
-                                  "this.value = " + (willRequire.isEmpty() ? io.getRead() : typeInJava + ".valueOf(" + io
-                                          .getRead() + ")") + ";",
+                new ExprFrom1<Block, Var>() {
+                    @Override
+                    public Block x(Var to) {
+                        return new Block(
+                                to.assign(asAValue("value")));
+                    }
+                },
+                new ExprFrom2<Block, Var, Var>() {
+                    @Override
+                    public Block x(Var to, Var from) {
+                        return new Block(to.assign(willRequire.isEmpty() ?
+                                        asAValue(io.getRead()) :
+                                        new MethodCall<AValue>(null, typeInJava + ".valueOf", io.getRead())));
+                    }
+                },
                                   io.getWrite((willRequire.isEmpty() ? "this.value" : "this.value.getNumber()")),
                                   io.getSize(),
+                BuiltIn.TO_STRING_OBJECT,
                                   false, willRequire);
     }
 
