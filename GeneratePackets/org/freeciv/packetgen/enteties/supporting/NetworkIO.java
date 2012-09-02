7c5502af5c76a1dbf8b62f831a49a1151cee8cca
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2012-09-02 21:15:30 +0200

    GeneratePackets: enteties: supporting: NetworkIO: Move generation of basic field type to more typed constructor.
     * Take size as an int to make it clear to anyone reading NetworkIO that no code is hiding in size.

--- GeneratePackets/org/freeciv/packetgen/enteties/supporting/NetworkIO.java
+++ GeneratePackets/org/freeciv/packetgen/enteties/supporting/NetworkIO.java
@@ -17,34 +17,42 @@ package org.freeciv.packetgen.enteties.supporting;
 import org.freeciv.packetgen.dependency.IDependency;
 import org.freeciv.packetgen.dependency.Requirement;
 import org.freeciv.packetgen.javaGenerator.HasAtoms;
+import org.freeciv.packetgen.javaGenerator.Var;
 import org.freeciv.packetgen.javaGenerator.expression.Block;
 import org.freeciv.packetgen.javaGenerator.expression.creators.ExprFrom1;
 import org.freeciv.packetgen.javaGenerator.expression.creators.Typed;
 import org.freeciv.packetgen.javaGenerator.expression.util.BuiltIn;
 import org.freeciv.packetgen.javaGenerator.expression.willReturn.AValue;
+import org.freeciv.packetgen.javaGenerator.expression.willReturn.AnInt;
 
 import java.util.Collection;
 import java.util.Collections;
 
 import static org.freeciv.packetgen.javaGenerator.expression.util.BuiltIn.asAValue;
+import static org.freeciv.packetgen.javaGenerator.expression.util.BuiltIn.asAnInt;
 
 // TODO: Quick: Split based on kind of fulfillment so a dummy don't have get for the nulls or throw excpetion on null
 // TODO: Long term: Split code in a better way between NetworkIO and java type. Some argument passing may fix all.
 public class NetworkIO implements IDependency {
     private final Requirement me;
-    private final String size;
+    private final int size;
     private final Typed<? extends AValue> readNoArgs;
     private final String write;
 
-    private NetworkIO(String type, String size, String write, Requirement.Kind kind, Typed<? extends AValue> readNoArgs) {
+    private NetworkIO(String type, int size, String write, Requirement.Kind kind, Typed<? extends AValue> readNoArgs) {
         this.me = new Requirement(type, kind);
         this.size = size;
         this.readNoArgs = readNoArgs;
         this.write = write;
     }
 
-    public String getSize() {
-        return "return " + size + ";";
+    public ExprFrom1<Typed<AnInt>, Var> getSize() {
+        return new ExprFrom1<Typed<AnInt>, Var>() {
+            @Override
+            public Typed<AnInt> x(Var arg1) {
+                return asAnInt(size + "");
+            }
+        };
     }
 
     public Block getRead(String argument, Typed<AValue> pre, Typed<AValue> post) {
@@ -81,7 +89,7 @@ public class NetworkIO implements IDependency {
      * @param read code to read an integer from a DataInput named "from"
      * @param write code to write an integer provided in braces right after to a DataOutput named "to"
      */
-    public static NetworkIO witIntAsIntermediate(String type, String size, String read, String write) {
+    public static NetworkIO witIntAsIntermediate(String type, int size, String read, String write) {
         return new NetworkIO(type, size, write, Requirement.Kind.FROM_NETWORK_TO_INT, BuiltIn.asAnInt(read));
     }
 
@@ -91,7 +99,7 @@ public class NetworkIO implements IDependency {
      */
     public static NetworkIO withBytesAsIntermediate(String type) {
         return new NetworkIO(type,
-             null,
+             -1,
              "to.write",
              Requirement.Kind.FROM_NETWORK_AMOUNT_OF_BYTES,
              null);
