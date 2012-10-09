fdae85aa1bf39e44ccf05972497e556c47c28000
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2012-10-09 12:03:28 +0200

    GeneratePackets: Java Generator: TargetMethod: Bug fix: Take call parameters as varargs, not an array.

--- GeneratePackets/org/freeciv/packetgen/javaGenerator/TargetMethod.java
+++ GeneratePackets/org/freeciv/packetgen/javaGenerator/TargetMethod.java
@@ -36,7 +36,7 @@ public class TargetMethod extends Address {
     }
 
     // TODO: Make return <? extends Returnable> when type system is fixed
-    public <Ret extends Returnable> MethodCall<Ret> call(Typed<? extends AValue>[] parameters) {
+    public <Ret extends Returnable> MethodCall<Ret> call(Typed<? extends AValue>... parameters) {
         return new MethodCall<Ret>(name, parameters);
     }
 }
