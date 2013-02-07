93407fef8adc581575a885c4fb5dfa9d1b6587a1
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-02-07 23:39:52 +0100

    Protocol: connection: Add PacketInputStream, a FilterInputStream that adds the ability to read packets.
     * move the reading of a packet from a stream from Uninterpreted's BackgroundReader to PacketInputStream

---
+++ Protocol/org/freeciv/connection/PacketInputStream.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2013 Sveinung Kvilhaugsvik.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.freeciv.connection;
+
+import org.freeciv.packet.PacketHeader;
+import org.freeciv.packet.RawPacket;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class PacketInputStream extends FilterInputStream {
+    private final Over state;
+    private final HeaderRead headerRead;
+
+    public PacketInputStream(InputStream in, Over state, final Class<? extends PacketHeader> packetHeaderClass) {
+        super(in);
+
+        this.state = state;
+        this.headerRead = new HeaderRead(packetHeaderClass);
+    }
+
+    public RawPacket readPacket() throws IOException, InvocationTargetException {
+        return readPacket(in, state, headerRead);
+    }
+
+    private static RawPacket readPacket(InputStream from, Over state, HeaderRead headerRead)
+            throws IOException, InvocationTargetException {
+        final byte[] headerStart = readXBytesFrom(headerRead.headerSize, from, state, true);
+        PacketHeader head;
+        try {
+            head = headerRead.headerReader.newInstance(new DataInputStream(new ByteArrayInputStream(headerStart)));
+        } catch (InstantiationException e) {
+            throw badHeader(e);
+        } catch (IllegalAccessException e) {
+            throw badHeader(e);
+        }
+
+        byte[] body = readXBytesFrom(head.getBodySize(), from, state, false);
+        return new RawPacket(body, head);
+    }
+
+    private static IllegalStateException badHeader(Exception e) {
+        return new IllegalStateException("Wrong data for reading headers", e);
+    }
+
+    private static byte[] readXBytesFrom(int wanted, InputStream from, Over state, boolean clean)
+            throws IOException {
+        byte[] out = new byte[wanted];
+        int alreadyRead = 0;
+        while(alreadyRead < wanted) {
+            final int bytesRead;
+            try {
+                bytesRead = from.read(out, alreadyRead, wanted - alreadyRead);
+            } catch (EOFException e) {
+                throw done(wanted, clean, alreadyRead);
+            }
+            if (0 <= bytesRead)
+                alreadyRead += bytesRead;
+            else if (state.isOver())
+                throw done(wanted, clean, alreadyRead);
+            Thread.yield();
+        }
+        return out;
+    }
+
+    private static IOException done(int wanted, boolean clean, int alreadyRead) throws DoneReading, EOFException {
+        if (clean && 0 == alreadyRead)
+            return new DoneReading("Nothing to read and nothing is waiting");
+        else
+            return new EOFException("Nothing to read and nothing is waiting." +
+                    "Read " + alreadyRead + " of " + wanted + " bytes");
+    }
+
+    private static class HeaderRead {
+        public final Constructor<? extends PacketHeader> headerReader;
+        public final int headerSize;
+
+        public HeaderRead(final Class<? extends PacketHeader> packetHeaderClass) {
+            try {
+                this.headerReader = packetHeaderClass.getConstructor(DataInput.class);
+                this.headerSize = packetHeaderClass.getField("HEADER_SIZE").getInt(null);
+            } catch (NoSuchMethodException e) {
+                throw new IllegalArgumentException("Could not find constructor for header interpreter", e);
+            } catch (NoSuchFieldException e) {
+                throw new IllegalArgumentException("Could not find header size in header interpreter", e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalArgumentException("Could not access header size in header interpreter", e);
+            }
+        }
+    }
+}
--- Protocol/org/freeciv/connection/Uninterpreted.java
+++ Protocol/org/freeciv/connection/Uninterpreted.java
@@ -19,8 +19,6 @@ import org.freeciv.packet.PacketHeader;
 import org.freeciv.packet.RawPacket;
 
 import java.io.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Map;
@@ -99,32 +97,19 @@ public class Uninterpreted implements FreecivConnection {
     }
 
     private static class BackgroundReader extends Thread {
-        private final InputStream in;
+        private final PacketInputStream in;
         private final LinkedList<RawPacket> buffered;
         private final Uninterpreted parent;
-        private final Constructor<? extends PacketHeader> headerReader;
-        private final int headerSize;
 
         private final ReflexPacketKind quickRespond;
 
         public BackgroundReader(InputStream in, Uninterpreted parent, ReflexPacketKind quickRespond,
                                 final Class<? extends PacketHeader> packetHeaderClass)
                 throws IOException {
-            this.in = in;
+            this.in = new PacketInputStream(in, parent, packetHeaderClass);
             this.parent = parent;
             this.quickRespond = quickRespond;
             this.buffered = new LinkedList<RawPacket>();
-
-            try {
-                headerReader = packetHeaderClass.getConstructor(DataInput.class);
-                headerSize = packetHeaderClass.getField("HEADER_SIZE").getInt(null);
-            } catch (NoSuchMethodException e) {
-                throw new IOException("Could not find constructor for header interpreter", e);
-            } catch (NoSuchFieldException e) {
-                throw new IOException("Could not find header size in header interpreter", e);
-            } catch (IllegalAccessException e) {
-                throw new IOException("Could not access header size in header interpreter", e);
-            }
 
             this.setDaemon(true);
         }
@@ -133,7 +118,7 @@ public class Uninterpreted implements FreecivConnection {
         public void run() {
             try {
                 while(true) {
-                    RawPacket incoming = readPacket();
+                    RawPacket incoming = in.readPacket();
 
                     quickRespond.handle(incoming);
 
@@ -152,44 +137,6 @@ public class Uninterpreted implements FreecivConnection {
             }
         }
 
-        private RawPacket readPacket()
-                throws IOException, InstantiationException, IllegalAccessException, InvocationTargetException {
-            final byte[] headerStart = readXBytesFrom(headerSize, in, parent, true);
-            PacketHeader head =
-                    headerReader.newInstance(new DataInputStream(new ByteArrayInputStream(headerStart)));
-
-            byte[] body = readXBytesFrom(head.getBodySize(), in, parent, false);
-            return new RawPacket(body, head);
-        }
-
-        private static byte[] readXBytesFrom(int wanted, InputStream from, Uninterpreted parent, boolean clean)
-                throws IOException {
-            byte[] out = new byte[wanted];
-            int alreadyRead = 0;
-            while(alreadyRead < wanted) {
-                final int bytesRead;
-                try {
-                    bytesRead = from.read(out, alreadyRead, wanted - alreadyRead);
-                } catch (EOFException e) {
-                    throw done(wanted, clean, alreadyRead);
-                }
-                if (0 <= bytesRead)
-                    alreadyRead += bytesRead;
-                else if (parent.isOver())
-                    throw done(wanted, clean, alreadyRead);
-                Thread.yield();
-            }
-            return out;
-        }
-
-        private static IOException done(int wanted, boolean clean, int alreadyRead) throws DoneReading, EOFException {
-            if (clean && 0 == alreadyRead)
-                return new DoneReading("Nothing to read and nothing is waiting");
-            else
-                return new EOFException("Nothing to read and nothing is waiting." +
-                        "Read " + alreadyRead + " of " + wanted + " bytes");
-        }
-
         public boolean hasPacket() {
             synchronized (buffered) {
                 return !buffered.isEmpty();
@@ -203,9 +150,4 @@ public class Uninterpreted implements FreecivConnection {
         }
     }
 
-    static class DoneReading extends IOException {
-        public DoneReading(String message) {
-            super(message);
-        }
-    }
 }
