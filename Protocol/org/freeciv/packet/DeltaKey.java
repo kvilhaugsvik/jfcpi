889b589ab9c0acfb88cf283808ba455047d5004d
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-02-18 01:06:08 +0100

    Protocol: packet: Add DeltaKey for use as a key in i HashMap to find the previous packet of the same kind where all key fields are equal.

---
+++ Protocol/org/freeciv/packet/DeltaKey.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2013. Sveinung Kvilhaugsvik
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.freeciv.packet;
+
+import org.freeciv.packet.fieldtype.FieldType;
+
+/**
+ * DeltaKey - use as key in a HashMap to find the previous packet of the same kind where all key fields are equal.
+ */
+public class DeltaKey {
+    private final int packetType;
+    private final FieldType<?>[] keys;
+
+    public DeltaKey(int packetType, FieldType<?>... keys) {
+        this.packetType = packetType;
+        this.keys = keys;
+    }
+
+    @Override
+    public boolean equals(Object to) {
+        if (!(to instanceof DeltaKey))
+            return false;
+
+        DeltaKey other = (DeltaKey) to;
+
+        if (other.packetType != this.packetType || other.keys.length != this.keys.length)
+            return false;
+
+        for (int i = 0; i < keys.length; i++)
+            if (!keys[i].equals(other.keys[i]))
+                return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int out = packetType << 16;
+
+        for (FieldType<?> key : keys)
+            out += key.hashCode();
+
+        return out;
+    }
+}
