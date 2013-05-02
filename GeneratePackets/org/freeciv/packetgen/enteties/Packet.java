ba62b6d4b9f45e72a6e6f3a0365e28ea2f14f6f0
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-05-02 15:57:51 +0200

    GeneratePackets: enteties: Packet: Importing all generated types are no longer needed in the generated packets as fully typed code now use their full address.

--- GeneratePackets/org/freeciv/packetgen/enteties/Packet.java
+++ GeneratePackets/org/freeciv/packetgen/enteties/Packet.java
@@ -82,7 +82,6 @@ public class Packet extends ClassWriter implements Dependency.Item, ReqKind {
                   List<Field> fields) throws UndefinedException {
         super(ClassKind.CLASS, TargetPackage.from(org.freeciv.packet.Packet.class.getPackage()),
                 Imports.are(Import.allIn(org.freeciv.packet.fieldtype.FieldType.class.getPackage()),
-                        Import.allIn(FCEnum.class.getPackage()),
                         Import.classIn(java.util.logging.Logger.class)),
                 "Freeciv's protocol definition", packetFlags, name,
                       DEFAULT_PARENT, Arrays.asList(TargetClass.from(org.freeciv.packet.Packet.class)));
