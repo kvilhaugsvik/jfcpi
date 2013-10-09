d9d4d41e8b44d162b2121aa0b8c90910d1195dbb
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-10-09 22:54:11 +0200

    Build system: Find the location of JavaGenerator's Java source files during bootstrap.

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 echo "Finding the source code"
 sh listFiles.sh Core
 sh listFiles.sh Utility
+sh listFiles.sh JavaGenerator
 sh listFiles.sh DependencyHandler
 
 echo "Generating the configure script and related files"
