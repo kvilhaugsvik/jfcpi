1689f39c389e1b2d9952054d41124e149b376810
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-05-15 21:03:39 +0200

    Documentation: DependencyHandler: Document Dependency.Item and Dependency.Maker

--- DependencyHandler/src/com/kvilhaugsvik/dependency/Dependency.java
+++ DependencyHandler/src/com/kvilhaugsvik/dependency/Dependency.java
@@ -17,14 +17,52 @@ package com.kvilhaugsvik.dependency;
 import java.util.*;
 
 public interface Dependency {
+    /**
+     * An Item can depend on other items and have other items depend on it.
+     */
     public static interface Item extends Dependency {
+        /**
+         * Other items that this item requires.
+         * @return the required items.
+         */
         public Collection<Requirement> getReqs();
+
+        /**
+         * The requirement this item fulfills.
+         * @return the requirement this item fulfills.
+         */
         public Requirement getIFulfillReq();
     }
 
+    /**
+     * A Maker can produce a {@link com.kvilhaugsvik.dependency.Dependency.Item} from other items. A Maker may be
+     * limited to produce a specific item. It may also produce different items depending on the input.
+     */
     public static interface Maker extends Dependency {
+        /**
+         * Get a list of the items needed to produce the requested item. The required items should be given as
+         * arguments to the {@link #produce(Requirement, com.kvilhaugsvik.dependency.Dependency.Item...)} method in the
+         * same order as they were requested.
+         *
+         * @param toProduce the wanted item.
+         * @return a list of the items needed to produce the wanted item.
+         */
         public List<Requirement> neededInput(Requirement toProduce);
+
+        /**
+         * Get a Required that matches each Requirement this Maker can produce. If a Requirement matches the method
+         * {@link #neededInput(Requirement)} should give a list of items required to produce it.
+         * @return a Required that matches each Requirement this Maker can produce.
+         */
         public Required getICanProduceReq();
+
+        /**
+         * Produce the requested item from the input items.
+         * @param toProduce the Requirement of the item that should be produced
+         * @param wasRequired the items required to produce it in the order given by {@link #neededInput(Requirement)}
+         * @return the requested Dependency.Item.
+         * @throws UndefinedException when the requested item couldn't be created.
+         */
         public Dependency.Item produce(Requirement toProduce, Dependency.Item... wasRequired) throws UndefinedException;
     }
 
