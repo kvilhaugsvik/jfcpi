5537532efb411693b6001f2cd5d68eaf1b47f27d
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-08-18 20:52:39 +0200

    GeneratePackets: Documentation: Add Javadoc to PacketHeaderKinds

--- GeneratePackets/src/org/freeciv/packetgen/PacketHeaderKinds.java
+++ GeneratePackets/src/org/freeciv/packetgen/PacketHeaderKinds.java
@@ -14,10 +14,26 @@
 
 package org.freeciv.packetgen;
 
+/**
+ * The kind of Freeciv protocol header. Does not include the delta header.
+ */
 public enum PacketHeaderKinds {
+    /**
+     * The header kind used in Freeciv 2.4 and 2.5. The header is 3 bytes long. The first two bytes are packet size. The
+     * last byte is the packet number.
+     */
     FC_2_4,
+
+    /**
+     * The header kind currently used in Freeciv trunk. The header changes size from 3 bytes in the beginning to 4 bytes
+     * when the connection is set up. The pre connection header is like {@see #FC_2_4}. The post connection header is
+     * like {@see #FC_2_4_99_2011_11_02}
+     */
     FC_trunk,
 
-    /* Never used in a release */
-    FC_2_4_99_2011_11_02
+    /**
+     * The header kind used for a time in trunk after Freeciv 2.4. The header is 4 bytes long. The first two bytes are
+     * packet size. The last two bytes are the packet number.
+     */
+    FC_2_4_99_2011_11_02 /* Never used in a stable release */
 }
