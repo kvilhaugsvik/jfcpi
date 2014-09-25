9ba346584e31414b30f6f55a8e7ee9e25f44b473
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-09-25 16:04:18 +0200

    Documentation: GeneratePackets: enteties: Complete SourceFile's Javadoc

--- GeneratePackets/src/org/freeciv/packetgen/enteties/SourceFile.java
+++ GeneratePackets/src/org/freeciv/packetgen/enteties/SourceFile.java
@@ -14,15 +14,28 @@ public class SourceFile implements Dependency.Item, ReqKind {
     private final String path;
     private final String content;
 
+    /**
+     * Construct a new SourceFile with the given path and content.
+     * @param path location of the source file.
+     * @param content content of the source file.
+     */
     public SourceFile(String path, String content) {
         this.path = path;
         this.content = content;
     }
 
+    /**
+     * Get the content of the source file.
+     * @return the content of the file.
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Get the path to the source file.
+     * @return the path to the source file.
+     */
     public String getPath() {
         return path;
     }
