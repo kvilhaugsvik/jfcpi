a71fc2018086cf43c7a37f2b33c7e86e5efeeb50
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-11-28 01:05:40 +0100

    Generated code: Field type arrays: pass array size to buffer2value()

--- GeneratePackets/src/org/freeciv/packetgen/enteties/supporting/TerminatedArray.java
+++ GeneratePackets/src/org/freeciv/packetgen/enteties/supporting/TerminatedArray.java
@@ -392,12 +392,13 @@ public class TerminatedArray extends FieldType {
                                 new Block(inc(outVar, kind.getAddress().newInstance(elem.ref(), helperParamLimits.ref()).callV("encodedLength")))),
                         RETURN(outVar.ref())));
 
+        Var<AValue> pSize = Var.param(int.class, "size");
         Var<AValue> pOld = Var.param(TargetClass.SELF_TYPED, "old");
         Var<AValue> pBuf = Var.param(TargetArray.from(kind.getAddress(), 1), "buf");
         Var<AValue> oVal = Var.local(type, "out", type.newInstance(pBuf.ref().<AnInt>callV("length")));
         Var<AnInt> count = Var.<AnInt>local(int.class, "i", literal(0));
         final Method.Helper buffer2value = Method.newHelper(Comment.no(), type, "buffer2value",
-                Arrays.<Var<?>>asList(pBuf, pOld),
+                Arrays.<Var<?>>asList(pBuf, pOld, pSize),
                 new Block(
                         oVal,
                         FOR(count, isSmallerThan(count.ref(), pBuf.ref().callV("length")), inc(count), new Block(
@@ -415,7 +416,7 @@ public class TerminatedArray extends FieldType {
                 new From2<Typed<AValue>, Typed<AValue>, Var>() {
                     @Override
                     public Typed<AValue> x(Typed<AValue> bytes, Var old) {
-                        return buffer2value.getAddress().call(bytes, old.ref());
+                        return buffer2value.getAddress().call(bytes, old.ref(), Hardcoded.pLimits.ref().callV("elements_to_transfer"));
                     }
                 },
                 new From2<Block, Var, Var>() {
