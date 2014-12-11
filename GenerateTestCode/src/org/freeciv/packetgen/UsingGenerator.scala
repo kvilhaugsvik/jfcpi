c744b2f75968d6e61e1739f91b1293e16b8780e9
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-12-11 13:51:13 +0100

    Test: Generated code: use the full generator to create field array test peers.

--- GenerateTestCode/src/org/freeciv/packetgen/UsingGenerator.scala
+++ GenerateTestCode/src/org/freeciv/packetgen/UsingGenerator.scala
@@ -26,6 +26,11 @@ class UsingGenerator {
   def caps() {
     UsingGenerator.caps(GeneratorDefaults.GENERATED_TEST_SOURCE_FOLDER)
   }
+
+  @Test
+  def diffArray() {
+    UsingGenerator.caps(GeneratorDefaults.GENERATED_TEST_SOURCE_FOLDER)
+  }
 }
 
 object UsingGenerator {
@@ -47,10 +52,24 @@ object UsingGenerator {
     full.writeToDir(generated_test_source_folder, true)
   }
 
+  /**
+   * Generate test peers for field array tests.
+   * @param generated_test_source_folder folder to write the generated test peers to.
+   */
+  def diffArray(generated_test_source_folder: String) {
+    var full = new GeneratePackets("GenerateTestCode/src/fieldArray/fieldArray.xml",
+      "GenerateTestCode/src/",
+      List[(String, String)](),
+      GeneratorDefaults.LOG_TO,
+      false)
+    full.writeToDir(generated_test_source_folder, true)
+  }
+
   def main(args: Array[String]) {
     val generated_test_source_folder = if (0 == args.length) GeneratorDefaults.GENERATED_TEST_SOURCE_FOLDER else args(0)
 
     simple(generated_test_source_folder)
     caps(generated_test_source_folder)
+    diffArray(generated_test_source_folder)
   }
 }
