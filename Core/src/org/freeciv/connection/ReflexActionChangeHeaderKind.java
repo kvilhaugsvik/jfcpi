6d48e9df08885adab2cb6fc106697bbfbcf52aff
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-05-08 21:03:01 +0200

    Core: connection: Add Javadoc to ReflexActionChangeHeaderKind

--- Core/src/org/freeciv/connection/ReflexActionChangeHeaderKind.java
+++ Core/src/org/freeciv/connection/ReflexActionChangeHeaderKind.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013. Sveinung Kvilhaugsvik
+ * Copyright (c) 2013 - 2015. Sveinung Kvilhaugsvik
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -16,9 +16,16 @@ package org.freeciv.connection;
 
 import org.freeciv.packet.PacketHeader;
 
+/**
+ * An action that changes the packet header kind.
+ */
 public class ReflexActionChangeHeaderKind implements ReflexReaction<PacketChangeHeader> {
     private final Class<? extends PacketHeader> to;
 
+    /**
+     * Create a new packet header kind changing action.
+     * @param to the header kind to change to.
+     */
     public ReflexActionChangeHeaderKind(Class<? extends PacketHeader> to) {
         this.to = to;
     }
